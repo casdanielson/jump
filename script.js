881e969f8f10b5c12d6d8f7f69aba50337a298e8
Cassandra <cassandraasdanielson@gmail.com>
2021-08-06 14:15:47 -0500

    Added additional counter functionality

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-
+var counter=0;
 function jump(){
-	if(character.classList != "animate"){
-		character.classList.add("animate");
-	}
-	setTimeout(function(){
-		character.classList.remove("animate");
-	},500);
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
 }
-
-var checkDead = setInterval(function){
-	var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-	var blockLeft = partseInt(window.getComputedStyle(block).getPropertyValue("left"));
-	if(blockLeft<20 && blockLeft>0 %% characterTOP>=130){
-		block.style.animation = "none";
-		block.style.display = "none";
-		alert("You LOSE!");
-	}
-},10);
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
